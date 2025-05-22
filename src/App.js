7309a012fe1173fcfdc58b58c569b71f81529a04
SaeedKarimian860 <SGK12190@gmail.com>
2025-05-21 23:02:46 -0400

    usepopcorn app

--- src/App.js
+++ src/App.js
@@ -53,17 +53,19 @@ const average = (arr) =>
 const KEY = "87b9ce6c";
 
 export default function App() {
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const tempQuery = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
       try {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`
+          `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${tempQuery}`
         );
 
         if (!res.ok)
@@ -86,7 +88,7 @@ export default function App() {
   return (
     <>
       <NavBar>
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
 
@@ -138,9 +140,7 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState("");
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"
