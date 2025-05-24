31e81d7bb3fa1d8fdf8016ea4d6470c53afe7b65
SaeedKarimian860 <SGK12190@gmail.com>
2025-05-24 19:44:04 -0400

    usepopcorn app

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const tempQuery = "interstellar";
+  const [selectedId, setSelectedId] = useState(null);
 
   /*
   useEffect(function () {
