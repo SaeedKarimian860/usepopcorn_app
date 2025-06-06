38d30bf7e1524dd87946bdbe8be4acd923e3de5a
SaeedKarimian860 <SGK12190@gmail.com>
2025-06-05 22:09:04 -0400

    usepopcorn app

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  
+
   // const [watched, setWatched] = useState([]);
-  const [watched, setWatched] = useState(function() {
-    const storedValue = localStorage.getItem('watched')
-    return storedValue
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
   });
 
   function handleSelectMovie(id) {
